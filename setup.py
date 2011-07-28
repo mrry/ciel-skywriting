e8dce2851203c888a10fef20903e6646e038c166
Derek Murray <Derek.Murray@cl.cam.ac.uk>
2011-07-28 22:14:22 +0000

    Initial factored-out Skywriting bindings.

--- setup.py
+++ setup.py
@@ -1,24 +1,17 @@
 from setuptools import setup
 
 setup(
-    name = "ciel",
+    name = "ciel-skywriting",
     version = '0.1-dev',
-    description = "Execution engine for distributed, parallel computation",
+    description = "Scripting language for distributed, parallel computation",
     author = "Derek Murray",
     author_email = "Derek.Murray@cl.cam.ac.uk",
-    url = "http://www.cl.cam.ac.uk/netos/ciel/",
-    packages = [ 'skywriting', 'skywriting.lang', 'skywriting.runtime',
-                 'skywriting.runtime.master', 'skywriting.runtime.worker',
-                 'skywriting.runtime.executors', 'skywriting.runtime.util', 'ciel', 'shared' ],
+    url = "http://www.cl.cam.ac.uk/netos/ciel/skywriting/",
+    packages = [ 'swi' ],
     package_dir = { '' : 'src/python' },
-    scripts = [ "scripts/%s" %s for s in
-                  ['ciel-killall.sh', 'ciel-kill-cluster', 'ciel-launch-cluster',
-                   'ciel-launch-master', 'ciel-launch-worker', 'ciel-poll-job',
-                   'ciel-print-job-result', 'ciel-run-job', 'ciel-run-job-async',
-                   'ciel-task-crawler', 'sw-job', 'sw-master', 'sw-start-job', 
-                   'sw-worker', 'skywriting'] ],
-    data_files = [ ("share/ciel/", ["src/python/skywriting/runtime/lighttpd.conf"]),
-                   ("share/ciel/skywriting/stdlib/",
+    entry_points = {'console_scripts' : ['skywriting=swi:main', 'sw-job=swi:cluster'],
+                    'ciel.executor.plugin' : ['swi=swi.executor:load']},
+    data_files = [ ("lib/ciel/skywriting/stdlib/",
                    ["src/sw/stdlib/%s" %s for s in
                      ["environ", "grab", "java", "mapreduce",
                       "stdinout", "sync"]])],
@@ -31,6 +24,6 @@ setup(
             'Topic :: Software Development :: Interpreters',
             'Topic :: System :: Distributed Computing',
         ],
-    requires=['simplejson', 'CherryPy (>=3.1.0)', 'ply', 'pycurl', 'httplib2' ]
+    requires=['ciel (>=0.1)', 'ply']
 )
 
